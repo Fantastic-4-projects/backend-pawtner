06523c522733de1d4966e9e3875b41b6091e3f97
Rifqi Darmawan <rifqidarmawan24@gmail.com>
2025-06-27 13:55:27 +0700

    initiate commit backend for pawtner final project

---
+++ src/main/java/com/enigmacamp/pawtner/PawtnerApplication.java
@@ -0,0 +1,13 @@
+package com.enigmacamp.pawtner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawtnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PawtnerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enigmacamp/pawtner/PawtnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enigmacamp.pawtner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PawtnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
