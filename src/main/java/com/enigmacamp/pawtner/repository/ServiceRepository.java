71d59935bfd71124705f744621c8f81ef202fe8c
Muh. Alrifqi Darmawan <rifqidarmawan24@gmail.com>
2025-07-01 08:46:43 +0000

    Revert "Merge branch 'dev/rifqi' into 'development'"
    This reverts merge request !5

--- src/main/java/com/enigmacamp/pawtner/repository/ServiceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.enigmacamp.pawtner.repository;
-
-import com.enigmacamp.pawtner.entity.Service;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ServiceRepository extends JpaRepository<Service, Integer> {
-}
